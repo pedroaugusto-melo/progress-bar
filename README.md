901421472d3a14ea71a15a0cb674c2155257d5a4
Pedro Augusto Melo <pedroaugusto.frmelo@gmail.com>
2022-01-13 16:28:18 -0300

    Project finished

---
+++ README.md
@@ -0,0 +1,30 @@
+# Progress Bar
+
+A Progress Bar.
+
+## Table of contents
+- [General info](#general-info)
+- [Objective](#objective)
+- [Technologies](#technologies)
+- [Setup](#setup)
+
+## General info
+
+A Progress Bar that increments its progress each second.
+
+## Objective
+
+Put in practice the concepts that I've learnt about HTML, CSS and JavaScript.
+
+## Technologies
+
+- CSS
+- HTML
+- JavaScript
+- Github
+- Git
+
+## Setup
+
+To run the project locally, download the project and open the index.html in a browser.
+
---
+++ script.js
@@ -0,0 +1,21 @@
+const progressBar = document.querySelector('.progress');
+const progressNum = document.querySelector('.percentage');
+
+function loadBar() {
+    progressBar.style.width = '1%';
+
+    const intervalId = setInterval(() => {
+        progressBar.style.width = `calc(${progressBar.style.width} + 1%)`;
+        progressNum.innerHTML = `${progressBar.style.width.slice(5, progressBar.style.width.length - 1)}`;
+    }, 100);
+
+    setTimeout(() => {
+        clearInterval(intervalId);
+    }, 9900);
+
+    setTimeout(() => {
+        window.location.reload();
+    }, 11000);
+}
+
+window.onload = loadBar;
